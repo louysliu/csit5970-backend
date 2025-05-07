5e9e5487c7ba168073edc95e4ea136980f09916f
Jiang <352826502@qq.com>
2025-05-08 01:21:28 +0800

    add run.py

---
+++ run.py
@@ -0,0 +1,4 @@
+from spark_yolo.spark_module import run_spark
+
+if __name__ == "__main__":
+    run_spark()
--- spark_yolo/spark_module.py
+++ spark_yolo/spark_module.py
@@ -12,16 +12,6 @@ from spark_yolo.record_db import update_redis_batch, update_postgreSQL_batch
 from spark_yolo.transmit import parse, decode_img
 from spark_yolo.yolo_module import yolo_detect
 
-# from config import *
-# from record_db import update_redis_batch, update_postgreSQL_batch
-# from transmit import parse, decode_img
-# from yolo_module import yolo_detect
-
-
-# connection pool. avoid repeated connection stuck
-
-# init redis
-# init once for each tasker
 _redis_conn = None
 def get_redis_connection():
     global _redis_conn
@@ -39,31 +29,28 @@ def get_pg_connection():
             host=PGSQL_HOST,
             port=PGSQL_PORT
         )
-# pg_pool = SimpleConnectionPool(
-#     minconn=1, maxconn=10, 
-#     dbname=PGSQL_DATABASE, user=PGSQL_USER, password=PGSQL_PWD, host=PGSQL_HOST
-# )
+
 
 def partition_yolo(partition_rows):
     redis_conn = get_redis_connection()
     pgsql_conn = get_pg_connection()
-    # pg_conn = pg_pool.getconn()
     buffer = []
     task_count = {}
     try:
         for row in partition_rows:
             try:
-                image = decode_img(row.image)
+                # print("start yolo")
+                image = decode_img(row["frameDATA"])
                 bboxes = yolo_detect(image)
             except Exception as e:
                 print(f"Error during YOLO: {e}")
             try:
-                buffer.append({"jobID": row.jobID, "frameID": row.frameID, "bboxes": bboxes})
-                task_count[row.jobID] = task_count.get(row.jobID, 0) + 1
+                buffer.append({"jobID": row["jobID"], "frameID": row["frameID"], "bboxes": bboxes})
+                task_count[row["jobID"]] = task_count.get(row["jobID"], 0) + 1
 
                 # regularly update database and clear buffer
                 if len(buffer) >= REDIS_UPDATE_FREQ:
-                    # update_postgreSQL_batch(pg_conn, buffer)
+                    update_postgreSQL_batch(pgsql_conn, buffer)
                     update_redis_batch(redis_conn, task_count)
                     buffer.clear()
                     task_count.clear()
@@ -72,25 +59,26 @@ def partition_yolo(partition_rows):
 
         # update rest results
         if buffer:
-            update_postgreSQL_batch(pg_conn, buffer)
+            update_postgreSQL_batch(pgsql_conn, buffer)
             update_redis_batch(redis_conn, task_count)
     finally:
         pass
-        # pg_pool.putconn(pg_conn)  # return the connection
 
-def run():
+def run_spark():
     # init session
+    # init spark session for streaming
     spark = SparkSession.builder \
-            .appName("YOLO") \
-            .master(SPARK_MASTER) \
-            .config("spark.jars.packages", KAFKA_VER) \
-            .getOrCreate()
+        .appName("YOLO") \
+        .master(SPARK_MASTER) \
+        .config("spark.jars.packages", KAFKA_VER) \
+        .getOrCreate()
     # load data  
-    raw_df = spark.readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "host1:port1,host2:port2") \
-    .option("subscribe", "topic-yolo") \
-    .load()
+    raw_df = spark\
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "host1:port1,host2:port2") \
+        .option("subscribe", "topic-yolo") \
+        .load()
     # df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
     value_df = raw_df.select(col("value"))
 
@@ -104,16 +92,11 @@ def run():
     parsed_df = value_df\
         .withColumn("parsed", parse_udf(col("value")))\
         .select("parsed.*")\
-        .repartition("jobID") # group by jobID
+        .partitionBy("jobID")
         
     
     parsed_df.rdd.foreachpartition(partition_yolo)
-    
-def test():
-    pass
 
-if __name__ == "__main__":
-    test()
 
 
 
